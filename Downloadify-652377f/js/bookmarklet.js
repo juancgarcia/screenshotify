4b6a521d00c3dc4101f4a065c48055f3b47fc04a
ide user ide_juancgarcia <juangarcia.info@gmail.com>
2012-06-14 19:28:46 +0000

    Splitting experimentation

--- Downloadify-652377f/js/bookmarklet.js
+++ Downloadify-652377f/js/bookmarklet.js
@@ -1,19 +1,14 @@
 (function() {
     /* options, customize to your needs */
     var title = 'screenshotify',
-    server = '//c9.io/juancgarcia/html2canvas/workspace/Downloadify-652377f' /*'//html2canvas.hertzen.com/js'*/,
+    server = 'https://raw.github.com/juancgarcia/screenshotify/master/Downloadify-652377f',
     proxy = '//html2canvas.appspot.com',
     debug = true,
     profile = false,
     scriptFiles = [
         '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
         'js/jquery.plugin.html2canvas',
-        'js/html2canvas',        
-		'js/swfobject',
-		'js/downloadify.min',
-        'http://stuartk.com/jszip/jszip',
-        'http://stuartk.com/jszip/jszip-deflate'/*,
-		'js/dZscript'*/
+        'js/html2canvas'
     ];
     
     var loader = {
@@ -44,7 +39,7 @@
                 }
                 _.head.appendChild(el);
                 ++_.index;
-                _.statusline.innerHTML = title+': loading "' + el.src + '" ' + _.index + ' / ' + _.files.length + '...';
+                _.statusline.innerHTML = title+': loading \'' + el.src + '\' ' + _.index + ' / ' + _.files.length + '...';
             }
             else {
                 _.statusline.parentNode.removeChild(_.statusline);
@@ -64,56 +59,21 @@
                 el.style.color = '#222';
                 el.style.backgroundColor = '#f99';
                 document.body.appendChild(el);
-                $(el).append('<div></div>', {id: 'downloadify'});
-                
-                /*
-                el.innerHTML = message;
-                */
-                var downloadSuccess = function() {
-                    jQuery(el).fadeOut('slow', function() {
-                        jQuery(this).remove();
-                    });
-                };
-                
-                var dlfyBtn,
-                getData = function(){                
-                    var zip = new JSZip();
-                    
-                    var imageData, baseSixFourData;
-                    if(window.screenGrab){
-                        imageData = window.screenGrab.toDataURL("image/png");
-                        baseSixFourData = imageData.substr(imageData.search(',')+1);
-                        
-                        zip.file('screenshot.png', baseSixFourData, {base64: true});
-                    }
-                    
-                	return zip.generate(); /*return document.getElementById('data').value;*/
-            	};
+                $(el).attr({id: 'dlfyWrapper'})/*.append($('<div></div>', {id: 'downloadify'}))*/;/*$('div#dlfyWrapper div#downloadify')*/
                 
                 $('#dlfyWrapper').hide();
-    
-                dlfyBtn = Downloadify.create('downloadify',{
-                	filename: function(){
-            			return 'download.zip';
-            		},
-                    dataType: 'base64',
-            		data: getData,
-            		onComplete: downloadSuccess,
-            		onCancel: function(){ ; },
-            		onError: function(){ ; },
-            		swf: server+'/'+'media/downloadify.swf',
-            		downloadImage: server+'/'+'images/download.png',
-            		width: 100,
-            		height: 30,
-            		transparent: true,
-            		append: false
-            	});
             
                 $(document.body).html2canvas({
                     /*flashcanvas: "../external/flashcanvas.min.js",*/
                     afterRender: function(canvas){
-                        window.screenGrab = canvas;
-                        $('#dlfyWrapper').show();
+                        //window.screenGrab = canvas;
+                        link = document.createElement('a');
+                        $('#dlfyWrapper').show().append(
+                            $(link).attr({id: 'screenshot', href: canvas.toDataURL() }).text('screenshot')
+                        );
+                        $('div#dlfyWrapper a#screenshot').bind('click', function(){
+                            $('div#dlfyWrapper').delay(2000).fadeOut(2000).delay(2000).remove();
+                        });
                     },
                     logging: true,
                     profile: true,
@@ -141,4 +101,4 @@
     statusline.style.zIndex = 9999999;
     document.body.appendChild(statusline);
     loader.onload();
-}());
+})();
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # screenshotify
 
 This is an experiment into the viability of combining
-(downloadify){https://github.com/dcneiner/Downloadify} with (html2canvas){http://html2canvas.hertzen.com/index.html}
+(downloadify)[https://github.com/dcneiner/Downloadify] with (html2canvas)[http://html2canvas.hertzen.com/index.html]
 and ultimately wrapping that functionality into a bookmarklet enabling users to take a snapshot of a webpage with minimal/zero server interaction
+
+
+In Downloadify/js there are two bookmarklet js files.
+
+bookmarklet.js is the plain version that provides uses html2canvas to generate a
+popup div housing data-uri link to the screenshot
+
+bookmarklet.jszip.downloadify.js is the downloadify experiment. Currently I'm having same-origin errors
+with the swf file that is obviously breaking the process. It may not be something that can be overcome
+in a bookmarklet context, but I'm going to see how close I can get.
+
+grab the bookmarklet (here)[http://jsfiddle.net/juancgarcia/bJEKv/]
